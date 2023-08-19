7c6ca33f0338729dccc2c5248fc3c3a1900002e7
seanc0der <sean.noko@gmail.com>
2023-08-19 03:33:47 +0200

    removed supplied codebase sample and added my own. The codebase produces the desired results as per the challenge rubric

--- scripts.js
+++ scripts.js
@@ -1,61 +1,70 @@
 const STATUS_MAP = {
-    shelf: {
-        color: 'green',
-        canReserve: true,
-        canCheckout: true,
-        canCheckIn: false,
-    },
-    reserved: {
-        color: 'blue',
-        canReserve: false,
-        canCheckout: true,
-        canCheckIn: false,
-    },
-    overdue: {
-        color: 'red',
-        canReserve: false,
-        canCheckout: false,
-        canCheckIn: true,
-    },
-    checkedOut: {
-        color: 'orange',
-        canReserve: false,
-        canCheckout: false,
-        canCheckIn: true,
-    }
+	shelf: {
+		color: "green",
+		canReserve: true,
+		canCheckout: true,
+		canCheckIn: false,
+	},
+	reserved: {
+		color: "blue",
+		canReserve: false,
+		canCheckout: true,
+		canCheckIn: false,
+	},
+	overdue: {
+		color: "red",
+		canReserve: false,
+		canCheckout: false,
+		canCheckIn: true,
+	},
+	checkedOut: {
+		color: "orange",
+		canReserve: false,
+		canCheckout: false,
+		canCheckIn: true,
+	},
+};
+
+// Edit below line
+
+const statusElements = document.querySelectorAll(".status");
+const checkinButtons = document.querySelectorAll(".checkin");
+
+checkinButtons.forEach((button) => {
+	button.style.color = "";
+});
+
+statusElements.forEach((statusElement) => {
+	let bookElement = statusElement.parentElement.parentElement;
+	let statusIndicator = statusElement.textContent;
+	toggleButtons(bookElement, statusIndicator);
+
+	switch (statusIndicator) {
+		case "shelf":
+			statusElement.style.color = STATUS_MAP.shelf.color;
+			break;
+		case "reserved":
+			statusElement.style.color = STATUS_MAP.reserved.color;
+			break;
+		case "overdue":
+			statusElement.style.color = STATUS_MAP.overdue.color;
+			break;
+		case "checkedOut":
+			statusElement.style.color = STATUS_MAP.checkedOut.color;
+			break;
+	}
+});
+
+function toggleButtons(bookElement, statusIndicator) {
+	const reserveButton = bookElement.querySelector(".reserve");
+	const checkoutButton = bookElement.querySelector(".checkout");
+	const checkinButton = bookElement.querySelector(".checkin");
+
+	const reserveToggled = STATUS_MAP[statusIndicator].canReserve;
+	const checkoutToggled = STATUS_MAP[statusIndicator].canCheckout;
+	const checkinToggled = STATUS_MAP[statusIndicator].canCheckIn;
+
+	reserveButton.disabled = !reserveToggled;
+	checkoutButton.disabled = !checkoutToggled;
+	checkinButton.disabled = !checkinToggled;
 }
-
-// Edit below line 
-
-status = selector(status)
-reserve = selector(reserve)
-checkout = selector(checkout)
-checkin = selector(checkin)
-
-status = selector(status)
-reserve = selector(reserve)
-checkout = selector(checkout)
-checkin = selector(checkin)
-
-status = selector(status)
-reserve = selector(reserve)
-checkout = selector(checkout)
-checkin = selector(checkin)
-
-checkin.0.color = none
-status.0.style.color = STATUS_MAP.status.color
-reserve.0 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-checkout.0 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-checkin.0 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
-
-checkin.1.color = none
-status.1.style.color = STATUS_MAP.status.color
-reserve.1 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-checkout.1 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-checkin.1 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
-
-checkin.2.color = none
-status.2.style.color = STATUS_MAP.status.color
-reserve.2 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-checkout.2 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-checkin.2 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
